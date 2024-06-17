b5ab9669dc4ac891d2040233ba899bf847e873bc
Rup-Royofficial <rupayanroy981@gmail.com>
2024-06-17 23:44:17 +0530

    working on controllers registerusers#1...

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,11 @@ import mongoose from 'mongoose'
 import { asyncHandler } from '../utils/asyncHandler.js'
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "Hello"
-    })
+    // res.status(200).json({
+    //     message: "Hello"
+    // })
+
+    const { fullName, email, password, username } = req.body
 } )
 
 export { registerUser }
